fe4ecbac6537da5453c89d4a8a0af4d8e3c28bdc
Slamadalius <slamasdalius@gmail.com>
2016-04-27 20:33:22 +0100

    adding id:3

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+   id: 1,
+   description: 'Meet friend for lunch',
+   completed: false
+},{
+   id: 2,
+   description: 'Go to market',
+   completed: false
+},{
+  id: 3,
+  description: 'Zu Zindsu',
+   completed: true
+}]
 
 app.get('/', function(req, res) {
    res.send('Todo API Root');
-})
+});
+
+app.get('/todos', function(req, res){
+   res.json(todos);
+});
 
 app.listen (PORT, function(){
    console.log('Express listening on port ' + PORT);
-})
+});
