cf7b16f4fbe2fc2d336602f61f7add9ba9a386eb
Slamadalius <slamasdalius@gmail.com>
2016-04-29 20:15:54 +0100

    Add PUT to todos

--- server.js
+++ server.js
@@ -36,6 +36,19 @@ app.get('/todos/:id', function(req, res){
 // POST /todos
 app.post('/todos', function(req, res){
    var body = _.pick(req.body, "description", "completed");
+   var validAttributes = {};
+   
+   if(body.hasOwnProperty("completed") && _.isBoolean(body.completed)){
+      validAttribute = body.completed;
+   } else if(body.hasOwnProperty('completed')) {
+      return res.status(400).send();
+   } else {
+      //never provided attribute no problem here
+   }
+   
+   
+   
+   
    
    
    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
@@ -64,6 +77,42 @@ app.delete('/todos/:id', function(req, res){
       res.json(matchedTodo);
    }
     
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+   var body = _.pick(req.body, "description", "completed");
+   var validAttributes = {};
+   var todoId = parseInt(req.params.id, 10);
+   var matchedTodo = _.findWhere(todos, {id:todoId});
+   
+   
+   if(!matchedTodo){
+      return res.status(400).send();
+   }
+   
+   if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+      validAttributes.completed = body.completed;
+   } else if(body.hasOwnProperty('completed')) {
+      return res.status(400).send();
+   } else {
+      //never provided attribute no problem here
+   }
+   
+   if(body.hasOwnProperty('description') && _.isString(body.description && body.description.trim().length > 0)){
+      validAttributes.description = body.description;
+   } else if(body.hasOwnProperty('description')) {
+      return res.status(400).send();
+   } else {
+      //never provided attribute no problem here
+   }
+   
+   // HERE
+   _.extend(matchedTodo, validAttributes);
+   res.json(matchedTodo);
+   
+   
+   
 });
 
 
