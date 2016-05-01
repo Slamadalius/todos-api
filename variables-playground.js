657dad20f0fa67d6bf576c9311cfd16fa6ce1cb5
Slamadalius <slamasdalius@gmail.com>
2016-05-01 16:34:10 +0100

    adding description search

---
+++ variables-playground.js
@@ -0,0 +1,22 @@
+//var person = {
+//   name: 'Dalius',
+//   age: 22
+//};
+//
+//function updatePerson (obj){
+//   obj.age = 21;
+//   debugger;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+var grades = [15, 88];
+
+function addGrades (grades) {
+   grades.push(55);
+   debugger;
+}
+
+addGrades(grades);
+console.log(grades);
